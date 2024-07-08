ed64d041cea991f07407a16c72a46d459702a420
Jaeha Yi <jaehaha1@gmail.com>
2024-07-07 22:42:54 -0600

    v1 done

--- Assets/Scripts/GameLobby.cs
+++ Assets/Scripts/GameLobby.cs
@@ -4,10 +4,17 @@ using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Lobbies;
 using Unity.Services.Lobbies.Models;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using System.Threading.Tasks;
+using Unity.Netcode; 
 using UnityEngine;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
 
 public class GameLobby : MonoBehaviour
 {
+    private const string KEY_RELAY_JOIN_CODE = "RelayJoinCode"; 
     public static GameLobby Instance { get; private set; }
 
     private Lobby joinedLobby; 
@@ -34,6 +41,52 @@ public class GameLobby : MonoBehaviour
         }
     }
 
+    private async Task<string> GetRelayJoinCode(Allocation allocation)
+    {
+        try
+        {
+            string relayJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+            return relayJoinCode;
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+
+            return default;
+        }
+    }
+
+    private async Task<Allocation> AllocateRelay()
+    {
+        try
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(GameMultiplayer.MAX_PLAYER_AMT - 1);
+
+            return allocation; 
+        } catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+
+            return default; 
+        }
+    }
+
+    private async Task<JoinAllocation> JoinRelay(string joincode)
+    {
+        try
+        {
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joincode);
+            return joinAllocation; 
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+
+            return default;
+        }
+    }
+
     public async void CreateLobby(string lobbyName, bool isPrivate)
     {
         try
@@ -42,9 +95,46 @@ public class GameLobby : MonoBehaviour
             {
                 IsPrivate = isPrivate,
             });
+
+            Allocation allocation = await AllocateRelay();
+
+            string relayJoinCode = await GetRelayJoinCode(allocation);
+
+            await LobbyService.Instance.UpdateLobbyAsync(joinedLobby.Id, new UpdateLobbyOptions
+            {
+                Data = new Dictionary<string, DataObject>
+                {
+                    { KEY_RELAY_JOIN_CODE, new DataObject(DataObject.VisibilityOptions.Member, relayJoinCode) }
+                }
+            }); 
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "dtls")); 
+
+            GameMultiplayer.Instance.StartHost();
+            Loader.LoadNetwork(Loader.Scene.CharacterSelectScene);
+
         } catch (LobbyServiceException e)
         {
             Debug.Log(e); 
+        }
+    }
+
+    public async void QuickJoin()
+    {
+        try
+        {
+            joinedLobby = await LobbyService.Instance.QuickJoinLobbyAsync();
+
+            string relayJoinCode = joinedLobby.Data[KEY_RELAY_JOIN_CODE].Value;
+
+            JoinAllocation joinAllocation = await JoinRelay(relayJoinCode);
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "dtls"));
+
+            GameMultiplayer.Instance.StartClient();
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
         }
     }
 }
---
+++ Assets/Scripts/LobbyUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class LobbyUI : MonoBehaviour
+{
+    [SerializeField] private Button createLobbyButton;
+    [SerializeField] private Button quickJoinButton;
+
+    private void Awake()
+    {
+        createLobbyButton.onClick.AddListener(() =>
+        {
+            GameLobby.Instance.CreateLobby("Lobby name", false); 
+        });
+        quickJoinButton.onClick.AddListener(() =>
+        {
+            GameLobby.Instance.QuickJoin();
+        });
+    }
+}
